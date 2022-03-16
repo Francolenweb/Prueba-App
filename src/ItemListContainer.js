17d0a0ae2108627d7dc5ed9dcc333ce4473520b7
Francolenweb <89050330+Francolenweb@users.noreply.github.com>
2022-03-16 19:27:14 -0300

    Add files via upload

---
+++ src/ItemListContainer.js
@@ -0,0 +1,45 @@
+import ItemList from "./ItemList"
+import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
+import { useParams } from "react-router-dom"
+import "./styles.css"
+
+const ItemListContainer = ({nombre }) => {
+ 
+  const [personajes, setPersonajes] = useState([])
+  const {idNombre} = useParams()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+        
+    fetch("https://rickandmortyapi.com/api/character")
+    .then((response)=>{
+        return response.json()
+    })
+    .then((resultado)=>{
+      setPersonajes(resultado.results)
+    })
+    .catch(()=>{
+        toast.error("Error al cargar los productos")
+    })
+    .finally(()=>{
+        setLoading(false)
+    })
+    
+  },[idNombre])
+
+  if(loading){
+    return <h1>Cargando...</h1>
+  }else{
+    return <div>
+              <h2>Bienvenido {nombre}!</h2>
+              <p>Esta es la pagina oficial de la tienda. Encontra todos nuestros productos aqui!</p>
+              <div className='cuerpo'>
+                
+                <ItemList personajes={personajes}/>
+              </div>
+            </div>
+  }
+}
+
+export default ItemListContainer
